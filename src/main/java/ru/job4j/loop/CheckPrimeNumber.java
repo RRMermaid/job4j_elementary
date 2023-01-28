ff78e3e9b99508c633cee03952ee5fd08dd31752
RRMermaid <rnizametdinova1991@gmail.com>
2023-01-29 01:23:00 +0500

    5.5. Простое число [#171694 #385840]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,23 +1,22 @@
 package ru.job4j.loop;
+
 import java.lang.*;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number < 2) {
-            return false;
-        }
-        if (number == 2) {
-            return true;
-        }
+        boolean prime = (number >= 2);
+
         if ((number & 1) == 0) {
-            return false;
-        }
-        int sqrt = ((int) Math.ceil(Math.sqrt(number)));
-        for (int index = 3; index <= sqrt; index += 2) {
-            if (number % index == 0) {
-                return false;
+            prime = (number == 2);
+        } else {
+            int sqrt = ((int) Math.ceil(Math.sqrt(number)));
+            for (int index = 3; index <= sqrt; index += 2) {
+                if (number % index == 0) {
+                    prime = false;
+                    break;
+                }
             }
         }
-        return true;
+        return prime;
     }
 }
