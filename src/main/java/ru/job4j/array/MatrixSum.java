6e418725d53799a86429894b0a316321aada327c
RRMermaid <rnizametdinova1991@gmail.com>
2025-01-11 01:44:31 +0500

    6.6.0. Двумерный массив [#226796]

--- src/main/java/ru/job4j/array/MatrixSum.java
+++ src/main/java/ru/job4j/array/MatrixSum.java
@@ -1,12 +1,12 @@
 package ru.job4j.array;
-
 public class MatrixSum {
     public static int sum(int[][] array) {
         int result = 0;
-        for (int[] row : array)
+        for (int[] row : array) {
             for (int val : row) {
                 result += val;
             }
+        }
         return result;
     }
 }
