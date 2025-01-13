5943752efd0dd41806f0ec60a7724853bf0fcf61
RRMermaid <rnizametdinova1991@gmail.com>
2025-01-13 21:49:35 +0500

    6.6.0. Двумерный массив [#226796]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -22,4 +22,12 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] result = new char[board.length];
+        for (int cell = 0; cell < board.length; cell++) {
+            result[cell] = board[cell][cell];
+        }
+        return result;
+    }
 }
