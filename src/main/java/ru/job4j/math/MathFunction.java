09811b02a84aa649883ea2d2dd060cda1e6fd111
RRMermaid <rnizametdinova1991@gmail.com>
2025-02-26 01:27:58 +0500

    6.6.0. Двумерный массив [#226796]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -4,15 +4,12 @@ public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
-
     public static double multiply(double first, double second) {
         return first * second;
     }
-
     public static double subtract(double first, double second) {
         return first - second;
     }
-
     public static double divide(double first, double second) {
         return first / second;
     }
