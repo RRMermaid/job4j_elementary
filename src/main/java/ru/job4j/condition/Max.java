9219653532121f5bca233428c103ccf9b52a85b0
RRMermaid <rnizametdinova1991@gmail.com>
2025-04-15 22:31:11 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,13 +1,23 @@
 package ru.job4j.condition;
 
 public class Max {
+
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int front) {
+        return max(max(left, right), front);
+    }
+
+    public static int max(int left, int right, int front, int back) {
+        return max(max(left, right, front), back);
+    }
+
+
     public static void main(String[] args) {
         System.out.println(max(9, 6));
-        System.out.println(max(3, 7));
-        System.out.println(max(5, 5));
+        System.out.println(max(3, 7, 3));
+        System.out.println(max(5, 5,7,9));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,40 +1,28 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
-import static ru.job4j.condition.Max.max;
-
-import org.junit.Assert;
-import ru.job4j.condition.Max;
 
 public class MaxTest {
 
     @Test
-    public void maxFrom9and6() {
+    public void whenMaxOfTwoNumbers() {
+        int result = Max.max(9, 6);
         int expected = 9;
-        int left = 9;
-        int right = 6;
-        int out = max(9, 6);
-        Assert.assertEquals(expected, out);
+        assertEquals(expected, result);
     }
 
     @Test
-    public void maxFrom3and7() {
+    public void whenMaxOfThreeNumbers() {
+        int result = Max.max(3, 7, 3);
         int expected = 7;
-        int left = 3;
-        int right = 7;
-        int out = max(3, 7);
-        Assert.assertEquals(expected, out);
+        assertEquals(expected, result);
     }
 
     @Test
-    public void maxFrom5and5() {
-        int expected = 5;
-        int left = 5;
-        int right = 5;
-        int out = max(5, 5);
-        assertThat(out).isEqualTo(expected);
+    public void whenMaxOfFourNumbers() {
+        int result = Max.max(5, 5, 7, 9);
+        int expected = 9;
+        assertEquals(expected, result);
     }
 }
