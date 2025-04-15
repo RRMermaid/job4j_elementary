9ffab76c26b6c6c40bbf32940aac0bb4fe4a4a19
RRMermaid <rnizametdinova1991@gmail.com>
2025-04-15 22:48:05 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,16 +7,16 @@ public class MaxTest {
 
     @Test
     public void testMaxTwoNumbers() {
-        assertEquals(9, Max.findMax(9, 6)); // Используем findMax вместо max
+        assertEquals(9, Max.findMax(9, 6));
     }
 
     @Test
     public void testMaxThreeNumbers() {
-        assertEquals(7, Max.findMax(3, 7, 3)); // Используем findMax вместо max
+        assertEquals(7, Max.findMax(3, 7, 3));
     }
 
     @Test
     public void testMaxFourNumbers() {
-        assertEquals(9, Max.findMax(5, 5, 7, 9)); // Используем findMax вместо max
+        assertEquals(9, Max.findMax(5, 5, 7, 9));
     }
 }
